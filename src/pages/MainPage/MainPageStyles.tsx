3bc8afe8ec66ec5bc127a28fbdf241a107649374
moudrey <moudryjan0@gmail.com>
2021-12-02 13:48:37 +0100

    Adding styles

---
+++ src/pages/MainPage/MainPageStyles.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet } from 'react-native';
+
+const mainPageStyles = StyleSheet.create({
+  main_view: {
+    width: '100%',
+    height: '100%',
+    backgroundColor: '#181818',
+  },
+  main_text: {
+    color: 'white',
+  },
+});
+
+export default mainPageStyles;
